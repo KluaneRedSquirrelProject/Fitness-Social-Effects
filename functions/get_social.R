af96470203e31cb2dd45ac001d39f3ccacc97ffc
Andrew McAdam <andrew.mcadam@colorado.edu>
2021-06-17 23:28:57 -0400

    updated function and Rmd
    I used dplyr instead of data.table to get the function to work for me.  IMPORTANT I also needed to exclude the focal individual from its own social environment.  This was done previously but was not in the function.  I was then able to replicate previous results.

--- functions/get_social.R
+++ functions/get_social.R
@@ -19,12 +19,13 @@ for (j in 1:n) {
   
   k = data.table(yr[j])
   
-  temp <- data1[gr_year == k$gr_year] #consider only those observations from the same grid and year
-  #AMc: I am getting an error here where for some reason it thinks gr_year is an objecct.  Can this be done in dplyr?
+  #temp <- data1[gr_year == k$gr_year] #consider only those observations from the same grid and year
+  #AMc: I am getting an error here where for some reason it thinks gr_year is an object.  Can this be done in dplyr?
   
-  #temp<-data1 %>% 
-  # filter (year == year[j],
-  #         grid == grid[j])
+  temp<-data1 %>% 
+    filter (year == year[j],
+            grid == grid[j],
+            squirrel_id != squirrel_id[j])
   
   temp$distance <- sqrt((30*(temp$locx - data1$locx[j]))^2 + (30*(temp$locy - data1$locy[j]))^2)
   
