ac84b7859910832012546019b3ac81f904d3bf96
Quinn Webber <webber.quinn@gmail.com>
2021-06-28 16:17:29 -0230

    Update get_social_perm.R

--- functions/get_social_perm.R
+++ functions/get_social_perm.R
@@ -5,30 +5,33 @@
 ### social effects function ###
 ################################
 
-get_social_perm <- function(data1, n, yr, dist, nn) {
+get_social_perm <- function(data1, n, dist, nn) {
   
   ## data1 = census_final data frame
   ## n = n in first loop
-  ## yr = data.table with list of unique gr_year levels
   ## dist = d_distance
   ## nn = vector of distance between all individuals
   
   data1$social_survival <- c()
   data1$social_repro <- c()
   
-for (j in 1:n) {
+for (j in 1:length(census_final$squirrel_id)){
   
-  temp<-data1 %>% 
+  temp<-census_final %>% 
     filter (year == year[j],
             grid == grid[j],
             squirrel_id != squirrel_id[j])
   
   n2<-length(temp$squirrel_id)  
   
-  temp$distance <- sample(nn$distance, n2) #sqrt((30*(temp$locx - data1$locx[j]))^2 + (30*(temp$locy - data1$locy[j]))^2)
+  temp2 <- df_nn %>% 
+    filter (year == year[j],
+            grid == grid[j])
+  
+  temp$distance <- sample(temp2$distance, n2) #sqrt((30*(temp$locx - data1$locx[j]))^2 + (30*(temp$locy - data1$locy[j]))^2)
   
   for (i in 1:n2) {        
-    temp$fraction[i] <- length(subset(dist, dist > temp$distance[i]))/length(dist)
+    temp$fraction[i] <- length(subset(d_distance, d_distance > temp$distance[i]))/length(d_distance)
   }
   
   temp$surv_frac <- temp$survived*temp$fraction
@@ -36,10 +39,10 @@ for (j in 1:n) {
   temp$surv_frac3 <- temp$survived3*temp$fraction
   temp$repro_frac <- temp$all_litters_fit*temp$fraction
   
-  data1$social_survival[j]<-sum(temp$surv_frac, na.rm=T)
-  data1$social_survival2[j]<-sum(temp$surv_frac2, na.rm=T)
-  data1$social_survival3[j]<-sum(temp$surv_frac3, na.rm=T)
-  data1$social_repro[j]<-sum(temp$repro_frac, na.rm=T)
+  census_final$social_survival[j]<-sum(temp$surv_frac, na.rm=T)
+  census_final$social_survival2[j]<-sum(temp$surv_frac2, na.rm=T)
+  census_final$social_survival3[j]<-sum(temp$surv_frac3, na.rm=T)
+  census_final$social_repro[j]<-sum(temp$repro_frac, na.rm=T)
 }
 
   data1
