7a6c8a8608880ac379416443f5bbdf7d2831793d
Quinn Webber <webber.quinn@gmail.com>
2021-07-30 11:01:15 -0230

    Update Fig4.R

--- code-for-figures/Fig4.R
+++ code-for-figures/Fig4.R
@@ -14,12 +14,12 @@ census_final_nonmast <- subset(census_final, mast == "n")
 census_final_mast <- subset(census_final, mast == "y")
 
 ## run models
-summary(fit5_mast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv2+std_soc_repro+(1|year)+(1|squirrel_id), 
+summary(fit5_mast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv3+std_soc_repro+(1|year)+(1|squirrel_id), 
                          data=census_final_mast, 
                          family=poisson, 
                          control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
 
-summary(fit5_nonmast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv2+std_soc_repro+(1|year)+(1|squirrel_id), 
+summary(fit5_nonmast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv3+std_soc_repro+(1|year)+(1|squirrel_id), 
                             data=census_final_nonmast,  #subset=!(year==2008&grid=="SU"), 
                             family=poisson, 
                             control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
@@ -27,15 +27,15 @@ summary(fit5_nonmast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv2+std_
 
 
 ## FIGURE 4
-vis_mast_surv <- visreg(fit5_mast, "std_soc_surv2", xlab="Survival of others", ylab="ARS - partial", xlim=c(-3.5, 3), ylim=c(-3.2, 2))
-vis_nonmast_surv <- visreg(fit5_nonmast, "std_soc_surv2", xlab="Survival of others", ylab="ARS - partial", xlim=c(-3.5, 3), ylim=c(-3.2, 2))
+vis_mast_surv <- visreg(fit5_mast, "std_soc_surv3", xlab="Survival of others", ylab="ARS - partial", xlim=c(-3.5, 3), ylim=c(-3.2, 2))
+vis_nonmast_surv <- visreg(fit5_nonmast, "std_soc_surv3", xlab="Survival of others", ylab="ARS - partial", xlim=c(-3.5, 3), ylim=c(-3.2, 2))
 
 
-fig4a <- ggplot(filter(vis_nonmast_surv$fit), aes(std_soc_surv2, visregFit))+
+fig4a <- ggplot(filter(vis_nonmast_surv$fit), aes(std_soc_surv3, visregFit))+
   geom_line(colour = 'black', 
             size=1)+
   geom_point(data = filter(vis_nonmast_surv$res),
-             aes(std_soc_surv2, visregRes), 
+             aes(std_soc_surv3, visregRes), 
              alpha = 0.8, 
              color = "#d8b365") +
   xlab('Survival of others') +
@@ -51,11 +51,11 @@ fig4a <- ggplot(filter(vis_nonmast_surv$fit), aes(std_soc_surv2, visregFit))+
         panel.background = element_blank(), 
         panel.border = element_rect(colour = "black", fill=NA, size = 1))
 
-fig4b <- ggplot(filter(vis_mast_surv$fit), aes(std_soc_surv2, visregFit))+
+fig4b <- ggplot(filter(vis_mast_surv$fit), aes(std_soc_surv3, visregFit))+
   geom_line(colour = 'black', 
             size=1)+
   geom_point(data = filter(vis_mast_surv$res),
-             aes(std_soc_surv2, visregRes), 
+             aes(std_soc_surv3, visregRes), 
              alpha = 0.8, 
              color = "#5ab4ac") +
   xlab('Survival of others') +
