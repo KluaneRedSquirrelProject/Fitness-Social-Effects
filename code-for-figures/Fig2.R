36cb4efd6e26193bc08a031326207c25cdd55e06
Quinn Webber <webber.quinn@gmail.com>
2021-09-30 13:41:25 -0230

    Update Fig2.R

--- code-for-figures/Fig2.R
+++ code-for-figures/Fig2.R
@@ -1,44 +1,43 @@
 
 
+
+
 library(ggplot2)
 library(gridExtra)
 library(visreg)
 library(lme4)
 library(dplyr)
 
-## load "census_final file from file from data/ 
+## load Social_Fitness_Data.RData file from file from data/ 
 
 ## subset to mast and non-mast years
 census_final_nonmast <- subset(census_final, mast == "n")
 census_final_mast <- subset(census_final, mast == "y")
 
 ## run models
-summary(fit_nonmast<-glmer(survived~age+I(age^2)+grid+std_soc_surv3+(1|year)+(1|squirrel_id), 
-                           data=census_final_nonmast, 
-                           family=binomial, 
-                           na.action=na.exclude, 
-                           control=glmerControl(optimizer="bobyqa", optCtrl=list(maxfun=2e5))))
-summary(fit_mast<-glmer(survived~age+I(age^2)+grid+std_soc_surv3+(1|year)+(1|squirrel_id), 
-                        data=census_final_mast, 
-                        family=binomial, 
-                        na.action=na.exclude, 
-                        control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+summary(fit4_mast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv3+std_soc_repro+(1|year)+(1|squirrel_id), 
+                    data=census_final_mast, 
+                    family=poisson, 
+                    control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+
+summary(fit4_nonmast<-glmer(all_litters_fit~age+I(age^2)+grid+std_soc_surv3+std_soc_repro+(1|year)+(1|squirrel_id), 
+                         data=census_final_nonmast,  #subset=!(year==2008&grid=="SU"), 
+                         family=poisson,  
+                         control=glmerControl(optimizer="bobyqa",optCtrl=list(maxfun=2e5))))
+
+vis_mast_repro <- visreg(fit4_mast, "std_soc_repro", xlab="ARS of others", ylab="ARS - partial", xlim=c(-2.5, 6), ylim=c(-3.3, 1.7))
+vis_nonmast_repro <- visreg(fit4_nonmast, "std_soc_repro", xlab="ARS of others", ylab="ARS - partial") #, xlim=c(-2.5, 6), ylim=c(-3.3, 1.7))
 
 
-## FIGURE 2
-vis_nonmast <- visreg(fit_nonmast, "std_soc_surv3", xlab="Survival of others", ylab="log odds (survival)", ylim=c(-1, 3), xlim=c(-3.2, 3.2))
-vis_mast <- visreg(fit_mast, "std_soc_surv3", xlab="Survival of others", ylab="log odds (survival)", ylim=c(-1, 3), xlim=c(-3.2, 3.2))
-
-
-fig2a <- ggplot(filter(vis_nonmast$fit), aes(std_soc_surv3, visregFit))+
+fig2a <- ggplot(filter(vis_nonmast_repro$fit), aes(std_soc_repro, visregFit))+
   geom_line(colour = 'black', 
             size=1)+
-  geom_point(data = filter(vis_nonmast$res),
-             aes(std_soc_surv3, visregRes), 
+  geom_point(data = filter(vis_nonmast_repro$res),
+             aes(std_soc_repro, visregRes), 
              alpha = 0.8, 
              color = "#d8b365") +
-  xlab('Survival of others') +
-  ylab('log odds (survival)') + 
+  xlab('ARS of others') +
+  ylab('ARS - partial') + 
   ggtitle('(A) Non-mast years') +
   theme(legend.position = c(0.75,0.8),
         legend.title = element_blank(),
@@ -50,15 +49,15 @@ fig2a <- ggplot(filter(vis_nonmast$fit), aes(std_soc_surv3, visregFit))+
         panel.background = element_blank(), 
         panel.border = element_rect(colour = "black", fill=NA, size = 1))
 
-fig2b <- ggplot(filter(vis_mast$fit), aes(std_soc_surv3, visregFit))+
+fig2b <- ggplot(filter(vis_mast_repro$fit), aes(std_soc_repro, visregFit))+
   geom_line(colour = 'black', 
             size=1)+
-  geom_point(data = filter(vis_mast$res),
-             aes(std_soc_surv3, visregRes), 
+  geom_point(data = filter(vis_mast_repro$res),
+             aes(std_soc_repro, visregRes), 
              alpha = 0.8, 
              color = "#5ab4ac") +
-  xlab('Survival of others') +
-  ylab('log odds (survival)') + 
+  xlab('ARS of others') +
+  ylab('ARS - partial') + 
   ggtitle('(B) Mast years') +
   theme(legend.position = c(0.75,0.8),
         legend.title = element_blank(),
@@ -70,6 +69,6 @@ fig2b <- ggplot(filter(vis_mast$fit), aes(std_soc_surv3, visregFit))+
         panel.background = element_blank(), 
         panel.border = element_rect(colour = "black", fill=NA, size = 1))
 
-png("figures/Fig2.png", width = 6000, height = 3000, units = "px", res = 600)
+png("figures/Fig2.png", width = 6000, height = 3000, units = "px", res = 1000)
 grid.arrange(fig2a,fig2b,nrow = 1)
 dev.off()
